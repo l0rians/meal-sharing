f425cb2e30bdaa63299c10c4b0d2ca509f450e27
Noah <lorians995@gmail.com>
2024-10-25 16:12:53 +0200

    removed console.logs

--- app/src/components/MealsList.jsx
+++ app/src/components/MealsList.jsx
@@ -4,18 +4,14 @@ import React, { useEffect, useState } from "react";
 import styles from "./MealsList.module.css";
 const MealsList = () => {
   const [meals, setMeals] = useState([]);
-  console.log("MealList called");
-  console.log(`meals state is ${meals}`);
 
   useEffect(() => {
     const fetchMeals = async () => {
       console.log("Calling fetchMeals");
       try {
         const response = await fetch("http://localhost:3001/api/meals");
-
-        console.log("Response received:", response);
         const data = await response.json();
-        console.log("Data received:", data);
+
         setMeals(data);
       } catch (error) {
         console.error("Error fetching meals:", error);
@@ -29,7 +25,6 @@ const MealsList = () => {
     <div className={styles.container}>
       {meals.length ? (
         meals.map((meal) => {
-          console.log("Rendering meal:", meal);
           return (
             <div className={styles.meal} key={meal.id}>
               <h3>{meal.title}</h3>
