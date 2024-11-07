3fba274e8330c0d24eca751a0321ba0aa43178f0
Noah <lorians995@gmail.com>
2024-11-07 22:04:51 +0100

    fix: now using useNavigate to navigate to /meals page

--- app/src/components/HomePage/HomePage.jsx
+++ app/src/components/HomePage/HomePage.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import MealsList from "../MealsList";
 import styles from "./HomePage.module.css";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const goToMeals = () => {
+    navigate("/meals");
+  };
+
   return (
     <>
       <div className={styles.homepageContainer}>
         <h1 className={styles.heading}>Welcome to Meal Sharing</h1>
         <h2>Find and Share Meals</h2>
         <MealsList limit={3} />
-        <button onClick={() => (window.location.href = "/meals")}>
-          See More Meals
-        </button>
+        <button onClick={goToMeals}>See More Meals</button>
       </div>
     </>
   );
